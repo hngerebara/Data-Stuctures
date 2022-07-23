3d7033172937a87a749b885fe84b0fe41af53805
Hope Ngerebara <h.ngerebara@gmail.com>
2022-07-23 10:26:33 +0100

    use builtin functions

--- arrays/stringReversal.js
+++ arrays/stringReversal.js
@@ -10,11 +10,22 @@ function reverseString(input) {
 
 	const result = [];
 
-	for (let i = input.length - 1; i >= 0; i--) { // Time complexity - 0(n)
+	for (let i = input.length - 1; i >= 0; i--) {
+		// Time complexity - 0(n)
 		result.push(input[i]); // Space complexity - 0(n)
 	}
 
 	return result.join('');
 }
 
-reverseString('dsfs');
+function reverseStringUsingBuiltInFn(input) {
+	return input.split('').reverse().join('');
+}
+
+function reverseStringUsingES6Fn(input) {
+	return [...input].reverse().join('');
+}
+
+reverseString('Hello please reverse me');
+reverseStringUsingBuiltInFn('Hello please reverse me');
+reverseStringUsingES6Fn('Hello please reverse me');
