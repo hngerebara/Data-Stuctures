440b2baa0df69306ee864062b147264fc3bce327
Hope Ngerebara <h.ngerebara@gmail.com>
2022-07-23 10:19:38 +0100

    add complexity

--- arrays/stringReversal.js
+++ arrays/stringReversal.js
@@ -4,17 +4,17 @@ function reverseString(input) {
 		return 'Invalid input';
 	}
 
-    if (input.length <= 2 ) {
+	if (input.length <= 2) {
 		return 'String length is too short';
 	}
 
-    const result = [];
+	const result = [];
 
-    for(let i = input.length-1; i >= 0; i--){
-        result.push(input[i]);
-    }
+	for (let i = input.length - 1; i >= 0; i--) { // Time complexity - 0(n)
+		result.push(input[i]); // Space complexity - 0(n)
+	}
 
-    return result.join('');
+	return result.join('');
 }
 
 reverseString('dsfs');
