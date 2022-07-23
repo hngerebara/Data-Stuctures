a49c1a1df7311bedff62b1a96a3de7fbd520bc99
Hope Ngerebara <h.ngerebara@gmail.com>
2022-07-23 11:40:31 +0100

    add validation

--- Hash-Tables/firstRecurringValue.js
+++ Hash-Tables/firstRecurringValue.js
@@ -1,14 +1,21 @@
 function firstRecurringValue(data) {
+	// validate input
+	if (!data) {
+		return 'No data entered';
+	}
+
 	let obj = {};
 
-	for (let i = 0; i < data.length; i++) { // 0(n)
+	for (let i = 0; i < data.length; i++) {
+		// 0(n)
 		if (obj[data[i]]) {
-			console.log(data[i], 'Found repeated');
 			return data[i];
 		} else {
 			obj[data[i]] = true;
+			return 'No repeated data';
 		}
 	}
 }
 
-firstRecurringValue([3, 4, 6, 4, 7, 9, 8, 5, 3]);
+const result = firstRecurringValue([3, 4, 6, 7, 9, 8, 5]);
+console.log(result, '==== result');
